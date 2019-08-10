1b180b770dec3f2f359c05d1d54934ef615cec2a
Leesssssssss <772592971@qq.com>
2019-08-10 19:11:16 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// es6的一些转义
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 移动设备上的浏览器默认会在用户点击屏幕大约延迟300毫秒后才会触发点击事件，这是为了检查用户是否在做双击。为了能够立即响应用户的点击事件，才有了FastClick
+import fastclick from 'fastclick'
+import './common/stylus/index.styl'
+
+Vue.config.productionTip = false
+fastclick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
